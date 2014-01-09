JuliaGraphics/Colors.jl

commit 30f947eebbc082b030231700b2823836a52b7c0b

radians2degrees is deprecated, replaced by rad2deg

src/Color.jl
@@ -404,7 +404,7 @@ end
 convert(::Type{LAB}, c::XYZ) = convert(LAB, c, WP_DEFAULT)
 
 function convert(::Type{LAB}, c::LCHab)
-    hr = degrees2radians(c.h)
+    hr = deg2rad(c.h)
     LAB(c.l, c.c * cos(hr), c.c * sin(hr))
 end
 
@@ -434,7 +434,7 @@ end
 convert(::Type{LUV}, c::XYZ) = convert(LUV, c, WP_DEFAULT)
 
 function convert(::Type{LUV}, c::LCHuv)
-    hr = degrees2radians(c.h)
+    hr = deg2rad(c.h)
     LUV(c.l, c.c * cos(hr), c.c * sin(hr))
 end
 
@@ -445,7 +445,7 @@ convert(::Type{LUV}, c::ColorValue) = convert(LUV, convert(XYZ, c))
 # -------------------
 
 function convert(::Type{LCHuv}, c::LUV)
-    h = radians2degrees(atan2(c.v, c.u))
+    h = rad2deg(atan2(c.v, c.u))
     while h > 360; h -= 360; end
     while h < 0;   h += 360; end
     LCHuv(c.l, sqrt(c.u^2 + c.v^2), h)
@@ -458,7 +458,7 @@ convert(::Type{LCHuv}, c::ColorValue) = convert(LCHuv, convert(LUV, c))
 # -------------------
 
 function convert(::Type{LCHab}, c::LAB)
-    h = radians2degrees(atan2(c.b, c.a))
+    h = rad2deg(atan2(c.b, c.a))
     while h > 360; h -= 360; end
     while h < 0;   h += 360; end
     LCHab(c.l, sqrt(c.a^2 + c.b^2), h)

commit 9d7f70abc084253143de6474b1231638fff21bda

Fix 0.2 breakage from 30f947eebbc082b030231700b2823836a52b7c0b

src/Color.jl
@@ -15,6 +15,10 @@ export ColorValue, color,
 abstract ColorValue
 typealias ColourValue ColorValue
 
+# Delete once 0.2 is no longer supported:
+if !isdefined(:rad2deg)
+  const rad2deg = radians2degrees
+end
 
 # Common Colorspaces
 # ------------------