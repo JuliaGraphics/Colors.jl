72bd5aabc40a1e54f105a32f3828aa69fba3f0b5
Daniel Jones <dcjones@cs.washington.edu>
2014-05-05 19:16:46 -0700

    Support parsing "rgba()", "hsla()", and "transparent".

--- src/Color.jl
+++ src/Color.jl
@@ -11,7 +11,7 @@ export ColorValue, color,
        RGBA, HSVA, HSLA, XYZA, LABA, LUVA, LCHabA, LCHuvA, DIN99A, DIN99dA, DIN99oA, LMSA, RGBA32,
        protanopic, deuteranopic, tritanopic,
        cie_color_match, distinguishable_colors,
-       colordiff, colordiff_din99, colordiff_din99d, colordiff_din99o
+       colordiff, colordiff_din99, colordiff_din99d, colordiff_din99o,
        MSC, sequential_palette, diverging_palette, colormap
 
 # Delete once 0.2 is no longer supported:
--- src/parse.jl
+++ src/parse.jl
@@ -9,6 +9,8 @@ const col_pat_hex1 = r"(#|0x)([[:xdigit:]])([[:xdigit:]])([[:xdigit:]])"
 const col_pat_hex2 = r"(#|0x)([[:xdigit:]]{2})([[:xdigit:]]{2})([[:xdigit:]]{2})"
 const col_pat_rgb  = r"rgb\((\d+%?),(\d+%?),(\d+%?)\)"
 const col_pat_hsl  = r"hsl\((\d+%?),(\d+%?),(\d+%?)\)"
+const col_pat_rgba = r"rgba\((\d+%?),(\d+%?),(\d+%?),(\d+(?:\.\d*)?%?)\)"
+const col_pat_hsla = r"hsla\((\d+%?),(\d+%?),(\d+%?),(\d+(?:\.\d*)?%?)\)"
 
 # Parse a number used in the "rgb()" or "hsl()" color.
 function parse_rgb_hsl_num(num::String)
@@ -19,12 +21,20 @@ function parse_rgb_hsl_num(num::String)
     end
 end
 
+# Parse a number used in the alpha field of "rgba()" and "hsla()".
+function parse_alpha_num(num::String)
+    if num[end] == '%'
+        return parsefloat(num[1:end-1]) / 100
+    else
+        return parsefloat(num)
+    end
+end
+
 
 # Parse a color description.
 #
 # This parses subset of HTML/CSS color specifications. In particular, everything
-# is supported but: "hsla()", "rgba()" (since there is no notion of transparency
-# in this library) "currentColor", and "transparent".
+# is supported but: "currentColor".
 #
 # It does support named colors (though it uses X11 named colors, which are
 # slightly different than W3C named colors in some cases), "rgb()", "hsl()",
@@ -66,7 +76,29 @@ function color(desc::String)
                    parse_rgb_hsl_num(mat.captures[3]))
     end
 
+    mat = match(col_pat_rgba, desc_)
+    if mat != nothing
+        return RGBA(parse_rgb_hsl_num(mat.captures[1]),
+                    parse_rgb_hsl_num(mat.captures[2]),
+                    parse_rgb_hsl_num(mat.captures[3]),
+                    parse_alpha_num(mat.captures[4]))
+    end
+
+    mat = match(col_pat_hsla, desc_)
+    if mat != nothing
+        return HSLA(parse_rgb_hsl_num(mat.captures[1]),
+                    parse_rgb_hsl_num(mat.captures[2]),
+                    parse_rgb_hsl_num(mat.captures[3]),
+                    parse_alpha_num(mat.captures[4]))
+    end
+
+
     desc_ = lowercase(desc_)
+
+    if desc_ == "transparent"
+        return RGBA(0.0, 0.0, 0.0, 0.0)
+    end
+
     if !haskey(color_names, desc_)
         error("Unknown color: ", desc)
     end
