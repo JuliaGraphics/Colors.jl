9b9f6ddefee84eaf3d7a4775c18d8d5c8abfd4e0
Steven G. Johnson <stevenj@mit.edu>
2014-08-25 14:11:54 -0400

    unify writemime for Matrix/Vector of colors, and transition to continuous gradient for a large enough array

--- src/display.jl
+++ src/display.jl
@@ -15,70 +15,29 @@ function writemime(io::IO, ::MIME"image/svg+xml", c::ColorValue)
         """)
 end
 
-
 function writemime{T <: ColorValue}(io::IO, ::MIME"image/svg+xml",
-                                    cs::AbstractVector{T})
-    n = length(cs)
-    if n > 18
-        swatch_size = 5
-    elseif n > 12
-        swatch_size = 10
-    else
-        swatch_size = 15
-    end
+                                    cs::AbstractVecOrMat{T})
+    m,n = ndims(cs) == 2 ? size(cs) : (1,length(cs))
 
-    pad = 1
+    xsize,xpad = n > 50 ? (250/n,0) : n > 18 ? (5.,1) : n > 12 ? (10.,1) : n > 1 ? (15.,1) : (25.,0)
+    ysize,ypad = m > 28 ? (150/m,0) : m > 14 ? (5.,1) : m > 9 ? (10.,1) : m > 1 ? (15.,1) : (25.,0)
+    
     write(io,
         """
         <?xml version="1.0" encoding="UTF-8"?>
         <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
          "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
         <svg xmlns="http://www.w3.org/2000/svg" version="1.1"
-             width="$(n*swatch_size)mm" height="25mm"
-             shape-rendering="crispEdges">
-        """)
-
-    for (i, c) in enumerate(cs)
-        write(io,
-            """
-            <rect x="$((i-1)*swatch_size)mm" width="$(swatch_size - pad)mm" height="100%"
-                  fill="#$(hex(c))" stroke="none" />
-            """)
-    end
-
-    write(io, "</svg>")
-end
-
-
-function writemime{T <: ColorValue}(io::IO, ::MIME"image/svg+xml",
-                                    cs::AbstractMatrix{T})
-    m, n = size(cs)
-
-    if n > 18
-        swatch_size = 5
-    elseif n > 12
-        swatch_size = 10
-    else
-        swatch_size = 15
-    end
-
-    pad = 1
-    write(io,
-        """
-        <?xml version="1.0" encoding="UTF-8"?>
-        <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
-         "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-        <svg xmlns="http://www.w3.org/2000/svg" version="1.1"
-             width="$(n*swatch_size)mm" height="$(m*swatch_size)mm"
+             width="$(n*xsize)mm" height="$(m*ysize)mm"
              shape-rendering="crispEdges">
         """)
 
     for i in 1:m, j in 1:n
-        c = cs[i, j]
+        c = ndims(cs) == 2 ? cs[i, j] : cs[j]
         write(io,
             """
-            <rect x="$((j-1)*swatch_size)mm" y="$((i-1)*swatch_size)mm"
-                  width="$(swatch_size - pad)mm" height="$(swatch_size - pad)mm"
+            <rect x="$((j-1)*xsize)mm" y="$((i-1)*ysize)mm"
+                  width="$(xsize - xpad)mm" height="$(ysize - ypad)mm"
                   fill="#$(hex(c))" stroke="none" />
             """)
     end
