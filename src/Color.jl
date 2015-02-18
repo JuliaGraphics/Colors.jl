a50da0910004b7ca9820897b226268aaed441298
Jiahao Chen <jiahao@mit.edu>
2015-02-18 02:52:53 -0500

    Clean up Color.jl
    Remove undefined export 'colour'
    
    Remove compatibility code for julia v0.2

--- src/Color.jl
+++ src/Color.jl
@@ -8,7 +8,7 @@ import Base: ==, convert, eltype, hex, isless, linspace, show, typemin, typemax,
 import Graphics: set_source, set_source_rgb, GraphicsContext
 
 export ColorValue, color,
-       ColourValue, colour,
+       ColourValue,
        AlphaColorValue, AbstractAlphaColorValue, AbstractRGB,
        RGB, HSV, HSL, XYZ, xyY, Lab, LAB, Luv, LUV, LCHab, LCHuv, DIN99, DIN99d, DIN99o, LMS, RGB24,
        RGBA, HSVA, HSLA, XYZA, xyYA, LabA, LuvA, LCHabA, LCHuvA, DIN99A, DIN99dA, DIN99oA, LMSA, ARGB32,
@@ -19,12 +19,6 @@ export ColorValue, color,
        colordiff, DE_2000, DE_94, DE_JPC79, DE_CMC, DE_BFD, DE_AB, DE_DIN99, DE_DIN99d, DE_DIN99o,
        MSC, sequential_palette, diverging_palette, colormap,
        colormatch, CIE1931_CMF, CIE1964_CMF, CIE1931J_CMF, CIE1931JV_CMF
-
-# Delete once 0.2 is no longer supported:
-if !isdefined(:rad2deg)
-  const rad2deg = radians2degrees
-  const deg2rad = degrees2radians
-end
 
 # The core; every other include will need these type definitions
 include("colorspaces.jl")
