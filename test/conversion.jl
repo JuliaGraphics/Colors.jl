88a2c96abc381b722c764498446a282e936c8315
Tim Holy <tim.holy@gmail.com>
2015-08-18 07:23:44 -0500

    Tests: avoid name conflict with `red` function

--- test/conversion.jl
+++ test/conversion.jl
@@ -1,9 +1,9 @@
-using Colors, FixedPointNumbers, Compat
+using Colors, FixedPointNumbers, Compat, JLD
 using Base.Test
 
 fractional_types = (RGB, BGR, RGB1, RGB4)  # types that support Fractional
 
-const red = color("red")
+const redF64 = color("red")
 const red24 = RGB24(0x00ff0000)
 const red32 = ARGB32(0xffff0000)
 for T in (Float64, Float32, Ufixed8)
@@ -11,20 +11,20 @@ for T in (Float64, Float32, Ufixed8)
     @test eltype(c) == T
     c64 = convert(RGB{Float64}, c)
     @test typeof(c64) == RGB{Float64}
-    @test c64 == red
-    cr = convert(RGB{T}, red)
+    @test c64 == redF64
+    cr = convert(RGB{T}, redF64)
     @test cr == c
 end
-@test RGB(1,0,0) == red
-@test RGB(convert(UInt8, 1),0,0) == red
-@test RGB(convert(UInt8, 1),convert(UInt8, 0),convert(UInt8, 0)) == red
-@test convert(RGB, red24) == red
+@test RGB(1,0,0) == redF64
+@test RGB(convert(UInt8, 1),0,0) == redF64
+@test RGB(convert(UInt8, 1),convert(UInt8, 0),convert(UInt8, 0)) == redF64
+@test convert(RGB, red24) == redF64
 
 for Cto in ColorTypes.parametric
     for Cfrom in ColorTypes.parametric
         for Tto in (Float32, Float64)
             for Tfrom in (Float32, Float64)
-                c = convert(Cfrom{Tfrom}, red)
+                c = convert(Cfrom{Tfrom}, redF64)
                 @test typeof(c) == Cfrom{Tfrom}
                 c1 = convert(Cto, c)
                 @test eltype(c1) == Tfrom
@@ -44,7 +44,7 @@ for Cto in ColorTypes.parametric
     for Cfrom in fractional_types
         for Tto in (Float32, Float64)
             for Tfrom in (Ufixed8, Ufixed10, Ufixed12, Ufixed14, Ufixed16)
-                c = convert(Cfrom{Tfrom}, red)
+                c = convert(Cfrom{Tfrom}, redF64)
                 @test typeof(c) == Cfrom{Tfrom}
                 if !(eltype_default(Cto) <: FixedPoint)
                     c1 = convert(Cto, c)
@@ -62,7 +62,7 @@ for Cto in fractional_types
     for Cfrom in ColorTypes.parametric
         for Tto in (Ufixed8, Ufixed10, Ufixed12, Ufixed14, Ufixed16)
             for Tfrom in (Float32, Float64)
-                c = convert(Cfrom{Tfrom}, red)
+                c = convert(Cfrom{Tfrom}, redF64)
                 @test typeof(c) == Cfrom{Tfrom}
                 c2 = convert(Cto{Tto}, c)
                 @test typeof(c2) == Cto{Tto}
@@ -71,20 +71,20 @@ for Cto in fractional_types
     end
 end
 
-ac = RGBA(red)
+ac = RGBA(redF64)
 
 @test convert(RGB, ac) == RGB(1,0,0)
 @test convert(RGB{Ufixed8}, ac) == RGB{Ufixed8}(1,0,0)
 @test convert(RGBA{Ufixed8}, ac) == RGBA{Ufixed8}(1,0,0,1)
-@test convert(HSVA, ac) == HSVA{Float64}(convert(HSV, red), 1.0)
-@test convert(HSVA{Float32}, ac) == HSVA{Float32}(convert(HSV{Float32}, red), 1.0f0)
-@test convert(RGBA, red) == ac
+@test convert(HSVA, ac) == HSVA{Float64}(convert(HSV, redF64), 1.0)
+@test convert(HSVA{Float32}, ac) == HSVA{Float32}(convert(HSV{Float32}, redF64), 1.0f0)
+@test convert(RGBA, redF64) == ac
 
 @test convert(ARGB32, ac) == ARGB32(0xffff0000)
 @test convert(Uint32, convert(ARGB32, ac)) == 0xffff0000
 @test convert(RGB24, RGB(0xffuf8,0x00uf8,0x00uf8)) == RGB24(0x00ff0000)
 @test convert(Uint32, convert(RGB24, RGB(0xffuf8,0x00uf8,0x00uf8))) == 0x00ff0000
-redhsv = convert(HSV, red)
+redhsv = convert(HSV, redF64)
 @test convert(RGB24, redhsv) == RGB24(0x00ff0000)
 
 @test convert(RGB{Ufixed8}, red24) == RGB{Ufixed8}(1,0,0)
