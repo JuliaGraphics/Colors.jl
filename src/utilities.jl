6c78f314b92efb10e3d62052ec947aa324ba076c
Martin Hofmann <hofmannmartin@users.noreply.github.com>
2016-09-28 19:01:08 +0200

    generalize linspace (#253)
    * generalize linspace
    
    I just realized that the function `linspace` can not be used to linear interpolate subtypes of `TransparentColor`.
    
    I propose to loosen the restrictions on `T` to allow such a usage.
    
    * generalized weighted_color_mean
    
    In order for the function linspace to be as general as possible the function `weighted_color_mean` had to be generalized.
    
    * added promotion for symmetry reasons + use mapc
    
    reworked code based on suggestions of @timholy
    
    * fixed typo in weighted_color_mean and prototype test added
    
    * added tests for wighted_color_mean
    
    * reworked linspace and added tests
    
    * fixed bug in linspace for julia-0.4
    
    * catch Gray{Bool} with DomainError in weighted_color_mean

--- src/utilities.jl
+++ src/utilities.jl
@@ -32,44 +32,30 @@ end
 hex(c::Color) = hex(convert(RGB, c))
 hex(c::Colorant) = hex(convert(ARGB, c))
 
-# weighted_color_mean(w1, c1, c2) gives a mean color "w1*c1 + (1-w1)*c2".
-for (T,a,b,c) in ((:RGB,:r,:g,:b), (:HSV,:h,:s,:v), (:HSL,:h,:s,:l),
-                  (:XYZ,:x,:y,:z), (:Lab,:l,:a,:b), (:LCHab,:l,:c,:h),
-                  (:Luv,:l,:u,:v), (:LCHuv,:l,:c,:h), (:LMS,:l,:m,:s))
-    @eval weighted_color_mean(w1::Real, c1::$T, c2::$T) =
-      let w2 = w1 >= 0 && w1 <= 1 ? 1 - w1 : throw(DomainError())
-          $T(c1.$a * w1 + c2.$a * w2,
-             c1.$b * w1 + c2.$b * w2,
-             c1.$c * w1 + c2.$c * w2)
-      end
-end
-
 """
     weighted_color_mean(w1, c1, c2)
 
-Returns a color that is the weighted mean of `c1` and `c2`, where `c1`
-has a weight 0 ≤ `w1` ≤ 1.
+Returns the color `w1*c1 + (1-w1)*c2` that is the weighted mean of `c1` and 
+`c2`, where `c1` has a weight 0 ≤ `w1` ≤ 1.
 """
-weighted_color_mean(w1::Real, c1::RGB24, c2::RGB24) =
-    convert(RGB24, weighted_color_mean(w1, convert(RGB, c1), convert(RGB, c2)))
+function weighted_color_mean(w1::Real, c1::Colorant, c2::Colorant)
+    weight1 = convert(promote_type(eltype(c1), eltype(c2)),w1)
+    weight2 = weight1 >= 0 && weight1 <= 1 ? oftype(weight1,1-weight1) : throw(DomainError())
+    mapc((x,y)->weight1*x+weight2*y, c1, c2)
+end
+function weighted_color_mean(w1::Real, c1::Gray{Bool}, c2::Gray{Bool})
+    # weighting of two Gray{Bool} would return different color type and therefore omitted
+    throw(DomainError())
+end
 
 """
     linspace(c1::Color, c2::Color, n=100)
 
-Generates `n` colors in a linearly interpolated ramp from `c1` to
-`c2`, inclusive, returning an `Array` of colors.
+Generates `n`>2 colors in a linearly interpolated ramp from `c1` to`c2`,
+inclusive, returning an `Array` of colors.
 """
-function linspace{T<:Color}(c1::T, c2::T, n=100)
-    a = Array(T, convert(Int, n))
-    if n == 1
-        a[1] = c1
-        return a
-    end
-    n -= 1
-    for i = 0:n
-        a[i+1] = weighted_color_mean((n-i)/n, c1, c2)
-    end
-    a
+function linspace{T<:Colorant}(c1::T, c2::T, n::Integer=100)
+    return T[weighted_color_mean(w1, c1, c2) for w1 in linspace(1.0,0.0,n)]
 end
 
 #Double quadratic Bezier curve
---
+++ test/utilities.jl
@@ -0,0 +1,53 @@
+using Colors, FixedPointNumbers, Base.Test
+
+# test utility function weighted_color_mean
+parametric2 = [GrayA,AGray32,AGray]
+parametric3 = ColorTypes.parametric3
+parametric4A = setdiff(subtypes(ColorAlpha),[GrayA])
+parametricA4 = setdiff(subtypes(AlphaColor),[AGray32,AGray])
+colortypes = vcat(parametric3,parametric4A,parametricA4)
+colorElementTypes = [Float16,Float32,Float64,BigFloat,UFixed8,UFixed10,UFixed12,UFixed14,UFixed16]
+
+for T in colorElementTypes
+    c1 = Gray{T}(1)
+    c2 = Gray{T}(0)
+    @test weighted_color_mean(0.5,c1,c2) == Gray{T}(0.5)
+end
+
+for C in parametric2
+    for T in colorElementTypes
+        C == AGray32 && (T=U8)
+	c1 = C(T(0),T(1))
+        c2 = C(T(1),T(0))
+	@test weighted_color_mean(0.5,c1,c2) == C(T(1)-T(0.5),T(0.5))
+    end
+end
+
+for C in colortypes
+    for T in (Float16,Float32,Float64,BigFloat)
+	if issubtype(C,Color)
+            c1 = C(T(1),T(1),T(0))
+            c2 = C(T(0),T(1),T(1))
+	    @test weighted_color_mean(0.5,c1,c2) == C(T(0.5),T(0.5)+T(1)-T(0.5),T(1)-T(0.5))
+	else
+            C == ARGB32 && (T=U8)
+            c1 = C(T(1),T(1),T(0),T(1))
+            c2 = C(T(0),T(1),T(1),T(0))
+	    @test weighted_color_mean(0.5,c1,c2) == C(T(0.5),T(0.5)+T(1)-T(0.5),T(1)-T(0.5),T(0.5))
+	end
+    end
+end
+
+# test utility function linspace
+# linspace uses weighted_color_mean which is extensively tested.
+# Therefore it suffices to test the function using gray colors.
+for T in colorElementTypes
+    c1 = Gray(T(1))
+    c2 = Gray(T(0))
+    linc1c2 = linspace(c1,c2,43)
+    @test length(linc1c2) == 43
+    @test linc1c2[1] == c1
+    @test linc1c2[end] == c2
+    @test linc1c2[22] == Gray(T(0.5))
+    @test typeof(linc1c2) == Array{Gray{T},1}
+end
