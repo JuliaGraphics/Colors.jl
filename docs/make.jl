bc7221505b0f70377c8cb1c9cd6ac522ae0c240b
Fredrik Ekre <fredrik.ekre@chalmers.se>
2018-09-14 23:34:51 +0200

    Misc fixes: (#327)
    - Enable Project.toml again
     - Use docs/Project.toml to build docs
     - Build docs from Julia v1.0
     - Move Test and JLD2 to test-only dependencies
     - Remove unused test/REQUIRE

--- docs/make.jl
+++ docs/make.jl
@@ -19,7 +19,7 @@ makedocs(
 deploydocs(
     repo = "github.com/JuliaGraphics/Colors.jl.git",
     target = "build",
-    julia  = "0.7",
+    julia  = "1.0",
     osname = "linux",
     deps = nothing,
     make = nothing
