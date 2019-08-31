fcb3a0bc57bc4984c1d78c7f2c9ec8419e1f3743
kimikage <kimikage.ceo@gmail.com>
2019-08-31 17:18:26 +0900

    Fix negative size swatches in output of show() (#341)
    Fix negative size error, Simplify SVG output and Add tests

--- src/display.jl
+++ src/display.jl
@@ -3,62 +3,68 @@
 
 const max_width = 180
 const max_height = 150
-const max_pixel_size = 25
+const max_swatch_size = 25
 
-function Base.show(io::IO, ::MIME"image/svg+xml", c::Color)
+function Base.show(io::IO, mime::MIME"image/svg+xml", c::Color)
+    write_declaration(io, mime)
     write(io,
         """
-        <?xml version="1.0" encoding="UTF-8"?>
-        <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
-         "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
         <svg xmlns="http://www.w3.org/2000/svg" version="1.1"
              width="25mm" height="25mm" viewBox="0 0 1 1">
-             <rect width="1" height="1"
-                   fill="#$(hex(c))" stroke="none"/>
+             <rect width="1" height="1" fill="#$(hex(c))" stroke="none"/>
         </svg>
         """)
+    flush(io) # return nothing
 end
 
-function Base.show(io::IO, ::MIME"image/svg+xml",
+function Base.show(io::IO, mime::MIME"image/svg+xml",
                    cs::AbstractVector{T}) where T <: Color
-    show(io, MIME"image/svg+xml"(), Base.ReshapedArray(cs, (1, length(cs)), ()))
+    show(io, mime, Base.ReshapedArray(cs, (1, length(cs)), ()))
 end
 
-function Base.show(io::IO, ::MIME"image/svg+xml",
+function Base.show(io::IO, mime::MIME"image/svg+xml",
                            cs::AbstractMatrix{T}) where T <: Color
     m, n = size(cs)
-    apsect_ratio = clamp(n / m,
-                         max_pixel_size / max_height,
-                         max_width / max_pixel_size)
-    pixel_aspect = apsect_ratio / (n / m)
-    scale_factor = min(max_width / (n * max_pixel_size * pixel_aspect),
-                       max_height / (m * max_pixel_size),
-                       1)
-    ysize = max_pixel_size * scale_factor
-    xsize = ysize * pixel_aspect
+    w = min(n * max_swatch_size, max_width)
+    h = min(m * max_swatch_size, max_height)
+    if max_width * m > max_height * n
+        w = max(h * n / m, max_swatch_size)
+    else
+        h = max(w * m / n, max_swatch_size)
+    end
 
-    xpad = n > 50 ? 0 : 1
-    ypad = m > 28 ? 0 : 1
+    simplify(x) = replace(string(round(x, digits=2)), r"(^0(?=\.))|(.0+$)"=>"")
+    sw = simplify(1 - (w / n < 3.6 ? 0 : n / w))
+    sh = simplify(1 - (h / m < 3.6 ? 0 : m / h))
 
+    comp(x) = round(x)==round(x, digits=2) ? Int(round(x)) : round(x, digits=2)
+    write_declaration(io, mime)
     write(io,
         """
-        <?xml version="1.0" encoding="UTF-8"?>
-        <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
-         "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
         <svg xmlns="http://www.w3.org/2000/svg" version="1.1"
-             width="$(n*xsize)mm" height="$(m*ysize)mm"
-             shape-rendering="crispEdges">
+             width="$(comp(w))mm" height="$(comp(h))mm"
+             viewBox="0 0 $n $m" preserveAspectRatio="none"
+             shape-rendering="crispEdges" stroke="none">
         """)
 
     for i in 1:m, j in 1:n
-        c = ndims(cs) == 2 ? cs[i, j] : cs[j]
+        c = hex(cs[i, j])
         write(io,
             """
-            <rect x="$((j-1)*xsize)mm" y="$((i-1)*ysize)mm"
-                  width="$(xsize - xpad)mm" height="$(ysize - ypad)mm"
-                  fill="#$(hex(c))" stroke="none" />
+            <rect width="$sw" height="$sh" x="$(j-1)" y="$(i-1)" fill="#$c" />
             """)
     end
 
     write(io, "</svg>")
+    flush(io) # return nothing
+end
+
+function write_declaration(io::IO, ::MIME"image/svg+xml")
+    write(io,
+        """
+        <?xml version="1.0" encoding="UTF-8"?>
+        <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
+         "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+        """)
+    flush(io) # return nothing
 end
--- test/colormaps.jl
+++ test/colormaps.jl
@@ -1,9 +1,5 @@
 @testset "Colormaps" begin
     @test length(colormap("RdBu", 100)) == 100
 
-    # test ability to add to previously shown array of colors - issue #328
-    a = [colorant"white", colorant"red"]
-    show(stdout, MIME"image/svg+xml"(), a)
-    push!(a, colorant"blue")
-    @test length(a) == 3
+    # TODO: add more tests
 end
