da286f02882f0dcbcc16ebedd0f72f593fb19cb5
kimikage <kimikage.ceo@gmail.com>
2019-09-14 09:16:21 +0900

    Move colormap-related tests to `test/colormaps.jl` and Add regression tests

--- test/algorithms.jl
+++ test/algorithms.jl
@@ -1,21 +1,6 @@
 using Test, Colors
 
 @testset "Algorithms" begin
-    @test isconcretetype(eltype(colormap("Grays")))
 
-    @test_throws ArgumentError colormap("Grays", N=10)
 
-    col = distinguishable_colors(10)
-    @test isconcretetype(eltype(col))
-    local mindiff
-    mindiff = Inf
-    for i = 1:10
-        for j = i+1:10
-            mindiff = min(mindiff, colordiff(col[i], col[j]))
-        end
-    end
-    @test mindiff > 8
-
-    cols = distinguishable_colors(1)
-    @test colordiff(distinguishable_colors(1, cols; dropseed=true)[1], cols[1]) > 50
 end
