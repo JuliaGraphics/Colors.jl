c4b4244ad49beec9b02131d8dc342a81b5f88ffe
Tim Holy <tim.holy@gmail.com>
2021-03-04 09:49:49 -0600

    Warn, not error, on precompile failures

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,3 +1,14 @@
+macro warnpcfail(ex::Expr)
+    modl = __module__
+    file = __source__.file === nothing ? "?" : String(__source__.file)
+    line = __source__.line
+    quote
+        $(esc(ex)) || @warn """precompile directive
+     $($(Expr(:quote, ex)))
+ failed. Please report an issue in $($modl) (after checking for duplicates) or remove this directive.""" _file=$file _line=$line
+    end
+end
+
 function _precompile_()
     ccall(:jl_generating_output, Cint, ()) == 1 || return nothing
     eltypes = (N0f8, N0f16, Float32, Float64)        # eltypes of parametric colors
@@ -7,23 +18,23 @@ function _precompile_()
     # conversions
     ## to RGB
     for T in eltypes, F in feltypes, C in (HSV,LCHab,LCHuv,Lab,Luv,XYZ)
-        @assert precompile(Tuple{typeof(convert),Type{RGB{T}},C{F}})
+        @warnpcfail precompile(Tuple{typeof(convert),Type{RGB{T}},C{F}})
     end
     ## to XYZ
     for T in feltypes, F in feltypes, C in (HSV,LCHab,LCHuv,Lab,Luv,XYZ,RGB)
-        @assert precompile(Tuple{typeof(convert),Type{XYZ{T}},C{F}})
+        @warnpcfail precompile(Tuple{typeof(convert),Type{XYZ{T}},C{F}})
     end
     for T in feltypes, F in (N0f8, N0f16)
-        @assert  precompile(Tuple{typeof(convert),Type{XYZ{T}},RGB{F}})
+        @warnpcfail  precompile(Tuple{typeof(convert),Type{XYZ{T}},RGB{F}})
     end
     # parse
-    @assert precompile(Tuple{typeof(parse),Type{ColorTypes.Colorant},String})
-    @assert precompile(Tuple{typeof(parse),Type{RGB{N0f8}},String})
-    @assert precompile(Tuple{typeof(parse),Type{RGBA{N0f8}},String})
+    @warnpcfail precompile(Tuple{typeof(parse),Type{ColorTypes.Colorant},String})
+    @warnpcfail precompile(Tuple{typeof(parse),Type{RGB{N0f8}},String})
+    @warnpcfail precompile(Tuple{typeof(parse),Type{RGBA{N0f8}},String})
     # colordiff
     for T in eltypes
-        @assert precompile(Tuple{typeof(colordiff),RGB{T},RGB{T}})
+        @warnpcfail precompile(Tuple{typeof(colordiff),RGB{T},RGB{T}})
     end
-    @assert precompile(Tuple{typeof(colormap),String})
-    @assert precompile(Tuple{typeof(distinguishable_colors),Int})
+    @warnpcfail precompile(Tuple{typeof(colormap),String})
+    @warnpcfail precompile(Tuple{typeof(distinguishable_colors),Int})
 end
