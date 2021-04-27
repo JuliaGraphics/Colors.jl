65c4e5a56e8702ae4b1389dcd02d546a21ed2f11
kimikage <kimikage.ceo@gmail.com>
2021-04-27 12:54:00 +0900

    Version 0.12.8 (#472)
    This adds the support for ColorTypes v0.11.
    This also modifies the CI settings and removes `@assert`s from the precompilation script.

--- src/precompile.jl
+++ src/precompile.jl
@@ -7,23 +7,23 @@ function _precompile_()
     # conversions
     ## to RGB
     for T in eltypes, F in feltypes, C in (HSV,LCHab,LCHuv,Lab,Luv,XYZ)
-        @assert precompile(Tuple{typeof(convert),Type{RGB{T}},C{F}})
+        precompile(Tuple{typeof(convert),Type{RGB{T}},C{F}})
     end
     ## to XYZ
     for T in feltypes, F in feltypes, C in (HSV,LCHab,LCHuv,Lab,Luv,XYZ,RGB)
-        @assert precompile(Tuple{typeof(convert),Type{XYZ{T}},C{F}})
+        precompile(Tuple{typeof(convert),Type{XYZ{T}},C{F}})
     end
     for T in feltypes, F in (N0f8, N0f16)
-        @assert  precompile(Tuple{typeof(convert),Type{XYZ{T}},RGB{F}})
+        precompile(Tuple{typeof(convert),Type{XYZ{T}},RGB{F}})
     end
     # parse
-    @assert precompile(Tuple{typeof(parse),Type{ColorTypes.Colorant},String})
-    @assert precompile(Tuple{typeof(parse),Type{RGB{N0f8}},String})
-    @assert precompile(Tuple{typeof(parse),Type{RGBA{N0f8}},String})
+    precompile(Tuple{typeof(parse),Type{ColorTypes.Colorant},String})
+    precompile(Tuple{typeof(parse),Type{RGB{N0f8}},String})
+    precompile(Tuple{typeof(parse),Type{RGBA{N0f8}},String})
     # colordiff
     for T in eltypes
-        @assert precompile(Tuple{typeof(colordiff),RGB{T},RGB{T}})
+        precompile(Tuple{typeof(colordiff),RGB{T},RGB{T}})
     end
-    @assert precompile(Tuple{typeof(colormap),String})
-    @assert precompile(Tuple{typeof(distinguishable_colors),Int})
+    precompile(Tuple{typeof(colormap),String})
+    precompile(Tuple{typeof(distinguishable_colors),Int})
 end
