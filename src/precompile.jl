a515eab6827429f84cca26abc8af35dcc9421489
kimikage <kimikage.ceo@gmail.com>
2021-08-15 09:38:32 +0900

    Mitigate inference problems in color conversions (#508)
    This uses the `cnvt` instead of the top-level `convert`,
    when the source and destination are both known colors.
    This also blocks the propagation of inference failures
    by adding type assertions to the result of `convert`.

--- src/precompile.jl
+++ src/precompile.jl
@@ -5,16 +5,18 @@ function _precompile_()
     pctypes = (Gray, RGB, AGray, GrayA, ARGB, RGBA)  # parametric colors
     cctypes = (Gray24, AGray32, RGB24, ARGB32)       # non-parametric colors
     # conversions
-    ## to RGB
-    for T in eltypes, F in feltypes, C in (HSV,LCHab,LCHuv,Lab,Luv,XYZ)
-        precompile(Tuple{typeof(convert),Type{RGB{T}},C{F}})
+    ## from/to XYZ
+    for T in feltypes, C in (HSV,LCHab,LCHuv,Lab,Luv)
+        precompile(Tuple{typeof(convert),Type{C{T}},XYZ{T}})
+        precompile(Tuple{typeof(convert),Type{XYZ{T}},C{T}})
     end
-    ## to XYZ
-    for T in feltypes, F in feltypes, C in (HSV,LCHab,LCHuv,Lab,Luv,XYZ,RGB)
-        precompile(Tuple{typeof(convert),Type{XYZ{T}},C{F}})
-    end
-    for T in feltypes, F in (N0f8, N0f16)
+    for T in feltypes, F in eltypes
+        precompile(Tuple{typeof(convert),Type{RGB{F}},XYZ{T}})
         precompile(Tuple{typeof(convert),Type{XYZ{T}},RGB{F}})
+    end
+    ## to RGB
+    for T in eltypes, F in feltypes, C in (HSV,LCHab,LCHuv,Lab,Luv)
+        precompile(Tuple{typeof(convert),Type{RGB{T}},C{F}})
     end
     # parse
     precompile(Tuple{typeof(parse),Type{ColorTypes.Colorant},String})
@@ -22,6 +24,8 @@ function _precompile_()
     precompile(Tuple{typeof(parse),Type{RGBA{N0f8}},String})
     # colordiff
     for T in eltypes
+        # Currently, there is a problem with `Float64` related to `cos`/`sin`.
+        T === Float64 && continue
         precompile(Tuple{typeof(colordiff),RGB{T},RGB{T}})
     end
     precompile(Tuple{typeof(colormap),String})
