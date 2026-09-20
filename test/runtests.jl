4cca0aea9ba89ae3a7ca5b95ac19c8873ebe3eb2
kimikage <kimikage.ceo@gmail.com>
2026-09-20 22:12:21 +0900

    Add Aqua tests (#581)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,17 @@
 using Colors, Test
-@test isempty(detect_ambiguities(Colors))
+using Aqua
+
+@testset "Aqua tests" begin
+    Aqua.test_all(Colors, piracies=false)
+
+    colorantnames = filter(names(ColorTypes, all=false)) do s
+                        isdefined(ColorTypes, s) || return false
+                        t = getfield(ColorTypes, s)
+                        return isa(t, Type) && t <: Colorant
+                    end
+    coloranttypes = map(s -> getfield(ColorTypes,s), colorantnames)
+    Aqua.test_piracies(Colors, treat_as_own=coloranttypes)
+end
 
 include("algorithms.jl")
 include("conversion.jl")
@@ -11,5 +23,6 @@ include("display.jl")
 include("parse.jl")
 include("utilities.jl")
 
+# cf. https://github.com/JuliaGraphics/Colors.jl/pull/513
 using AbstractTrees
 @test isempty(detect_ambiguities(Colors))
